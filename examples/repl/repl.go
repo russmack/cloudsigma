31a66cd8add948570491893954bee9a852735765
russmack <russ_mack@hotmail.com>
2015-05-01 17:48:23 +0100

    Added config.

--- examples/repl/repl.go
+++ examples/repl/repl.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"github.com/russmack/cloudsigma"
 	"github.com/russmack/replizer"
+	"os"
+)
+
+var (
+	config *cloudsigma.Config
 )
 
 func main() {
+	config = cloudsigma.NewConfig()
+	_, err := config.Load()
+	if err != nil {
+		fmt.Println("Unable to load config.", err)
+		os.Exit(1)
+	}
+
 	// Create the repl, add command state machines, and start the repl.
 	repl := replizer.NewRepl()
 	repl.Name = "CloudSigma IaaS"
 	// Create a statemachine per command available in the repl.
 	repl.InstrMachineMap["cloud status"] = NewMachineCloudStatus().Start
+	repl.InstrMachineMap["usage"] = NewMachineUsage().Start
 	repl.InstrMachineMap["create server"] = NewMachineCreateServer().Start
 	repl.Start()
 }
