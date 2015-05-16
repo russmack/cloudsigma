23b9d1672bfa1b44853d8b773e0bcf05fddb24b8
russmack <russ_mack@hotmail.com>
2015-05-16 19:59:50 +0100

    Fixed missing pointers.

--- profile.go
+++ profile.go
@@ -6,18 +6,19 @@ const (
 
 // Profile
 type Profile struct {
-	Args Args
+	Args *Args
 }
 
 // NewProfile returns a Profile object.
 func NewProfile() *Profile {
 	o := Profile{}
-	o.Args = Args{Resource: EndpointProfile}
+	o.Args = NewArgs()
+	o.Args.Resource = EndpointProfile
 	return &o
 }
 
 // List returns the args required for a Profile GET request.
-func (o *Profile) List() Args {
+func (o *Profile) List() *Args {
 	o.Args.Verb = "GET"
 	o.Args.RequiresAuth = true
 	return o.Args
