530f73012c3fa27316cb09a78cba645c22d3f057
russmack <russ_mack@hotmail.com>
2015-05-16 20:25:18 +0100

    Added create, edit, delete.

--- servers.go
+++ servers.go
@@ -8,6 +8,12 @@ const (
 type Servers struct {
 	Args *Args
 }
+type ServerRequest struct {
+	Name        string `json:"name"`
+	Cpu         int    `json:"cpu"`
+	Memory      int    `json:"mem"`
+	VncPassword string `json:"vnc_password"`
+}
 
 // NewServers returns a Servers object.
 func NewServers() *Servers {
@@ -21,5 +27,35 @@ func NewServers() *Servers {
 func (o *Servers) List() *Args {
 	o.Args.Verb = "GET"
 	o.Args.RequiresAuth = true
+	return o.Args
+}
+
+// Create returns the args required to create a server.
+func (o *Servers) Create(servers []ServerRequest) *Args {
+	o.Args.Verb = "POST"
+	o.Args.RequiresAuth = true
+	o.Args.AddHeaderPair("Content-Type", "application/json")
+	body := struct {
+		Objects []ServerRequest `json:"objects"`
+	}{servers}
+	o.Args.Body = body
+	return o.Args
+}
+
+// Edit returns the args required to update a server.
+func (o *Servers) Edit(objectId string, server ServerRequest) *Args {
+	o.Args.Verb = "PUT"
+	o.Args.RequiresAuth = true
+	o.Args.AddHeaderPair("Content-Type", "application/json")
+	o.Args.ObjectId = objectId
+	o.Args.Body = server
+	return o.Args
+}
+
+// Delete returns the args required to delete a server.
+func (o *Servers) Delete(objectId string) *Args {
+	o.Args.Verb = "DELETE"
+	o.Args.RequiresAuth = true
+	o.Args.ObjectId = objectId
 	return o.Args
 }
