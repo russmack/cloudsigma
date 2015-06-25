fbd56a66099b2ec1c5cbc462abb60390922a92cd
russmack <russ_mack@hotmail.com>
2015-06-25 16:01:11 +0100

    Added get single vlan.  Fixed detailed call.

--- vlans.go
+++ vlans.go
@@ -8,6 +8,8 @@ const (
 type Vlans struct {
 	Args *Args
 }
+type VlanRequest struct {
+}
 
 // NewVlans returns a Vlans object.
 func NewVlans() *Vlans {
@@ -17,17 +19,25 @@ func NewVlans() *Vlans {
 	return &o
 }
 
-// NewVlansDetailed returns a Vlans object with a VlansDetailed endpoint.
-func NewVlansDetailed() *Vlans {
-	o := Vlans{}
-	o.Args = NewArgs()
-	o.Args.Resource = EndpointVlans + "/detail"
-	return &o
-}
-
 // NewList returns the args required for a Vlans GET request.
 func (o *Vlans) NewList() *Args {
 	o.Args.Verb = "GET"
 	o.Args.RequiresAuth = true
+	return o.Args
+}
+
+// NewListDetailed returns the args required for a Vlans GET request.
+func (o *Vlans) NewListDetailed() *Args {
+	o.Args.Verb = "GET"
+	o.Args.RequiresAuth = true
+	o.Args.Resource += "/detail"
+	return o.Args
+}
+
+// NewGet returns the args required for a Vlans GET request.
+func (o *Vlans) NewGet(uuid string) *Args {
+	o.Args.Verb = "GET"
+	o.Args.RequiresAuth = true
+	o.Args.Resource += "/" + uuid + "/"
 	return o.Args
 }
